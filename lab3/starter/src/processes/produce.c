11abee61c420721d0e147d090689ce637eaa3398
Colin Mackenzie Brum <cmbrum@ecelinux4.uwaterloo.ca>
2018-11-17 13:33:20 -0500

    Reverting processes to working

--- lab3/starter/src/processes/produce.c
+++ lab3/starter/src/processes/produce.c
@@ -47,10 +47,10 @@ int main(int argc, char *argv[])
 	num_c = atoi(argv[4]);  /* number of consumers        */
 
 	mq_unlink("/coolqueue");
-        attr.mq_msgsize = 20;
-        attr.mq_msgsize = maxmsg;
-        queue_d = mq_open("/coolqueue", O_RDWR | O_CREAT, 0644, &attr);
-        printf("Open desc %d\n", queue_d);
+ 	attr.mq_maxmsg = 10;
+    attr.mq_msgsize = 20;
+    queue_d = mq_open("/coolqueue", O_RDWR | O_CREAT, 0644, &attr);
+    printf("Open desc %d\n", queue_d);
 
    
 	pini = fork();
@@ -58,11 +58,15 @@ int main(int argc, char *argv[])
 		return -1;
 	}else if ( pini == 0){
     	/*spawner p*/
+    	printf("Producer spawner\n");
     	pid_t pros_pids[num_p];
 
     	for(int i = 0; i < num_p; i++){
+    		printf("I'm spawning producers\n");
     		pros_pids[i] = fork();
+    		printf("Forked pros_pid is %d\n", pros_pids[i]);
             if (pros_pids[i] == 0) {
+                printf("Producer\n");
                 producer(i);
             } else {
                 return 0;
@@ -75,34 +79,42 @@ int main(int argc, char *argv[])
     		return -1;
     	} else if (cini == 0){
     		/*spwaner c*/
+    		printf("Consumer spwaner\n");
             pid_t cons_pids[num_c];
 
             for (int i = 0; i < num_c; i++) {
+                printf("I'm spawning consumers\n");
                 cons_pids[i] = fork();
+                printf("Forked cons_pid is %d\n", cons_pids[i]);
                 if (cons_pids[i] == 0) {
                     consumer_id = i;
+                    printf("Consumer id %d spawned.\n", consumer_id);
                     mqd_t recv = mq_open("/coolqueue", O_RDONLY);
                     consumer(recv);
                 }
             }
     
     	}else{
-    	    gettimeofday(&tv, NULL);
-    	    g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
-
-
-    	    gettimeofday(&tv, NULL);
-    	    g_time[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
-
-    	    printf("System execution time: %.6lf seconds\n", \
-            g_time[1] - g_time[0]);
-    	    exit(0);
-	}
+    		/*parent main*/
+    	}
     }
+
+
+    gettimeofday(&tv, NULL);
+    g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
+
+
+    gettimeofday(&tv, NULL);
+    g_time[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
+
+    printf("System execution time: %.6lf seconds\n", \
+            g_time[1] - g_time[0]);
+    exit(0);
 }
 
 void producer(int i){	
     mqd_t send = mq_open("/coolqueue", O_WRONLY);
+    printf("Send desc %d\n", send);
 
     char msg[num];
     for (int j = i; j < num; j += num_p){
@@ -115,17 +127,16 @@ void producer(int i){
 void consumer(mqd_t fd) {
     int received;
     int root;
+    printf("Recv desc %d\n", fd);
     char rcv[40];
     while(1) {
         mq_receive(fd, rcv, 40, NULL);
         received = atoi(rcv);
+        printf("Consumer %d Received int %d", consumer_id, received);
         root = sqrt(received);
         if ((root * root) == received) {
                 printf("%d %d %d\n", consumer_id, received, root);
-		if (received == num - 1) {
-			return;
-		}
         }
-   	
     }
+    perror(NULL);
 }
