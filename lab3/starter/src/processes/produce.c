8531101bfabfe54f5ffc2df4266d56337d6fa0b4
Colin Mackenzie Brum <cmbrum@ecelinux4.uwaterloo.ca>
2018-11-17 15:22:02 -0500

    Reworking processes

--- lab3/starter/src/processes/produce.c
+++ lab3/starter/src/processes/produce.c
@@ -26,83 +26,71 @@ int consumer_id;
 struct timeval tv;
 pid_t pini;
 mqd_t queue_d;
-struct mq_attr attr;
 
-void producer(int);
-void consumer(mqd_t fd);
+void producer(int prod_id);
+void consumer(mqd_t fd, int cons_id);
 
-int main(int argc, char *argv[])
-{
-	
+int main(int argc, char *argv[]) {
 
-
-	if (argc != 5) {
-		printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
-		exit(1);
-	}
-
-	num = atoi(argv[1]);	/* number of items to produce */
-	maxmsg = atoi(argv[2]); /* buffer size                */
-	num_p = atoi(argv[3]);  /* number of producers        */
-	num_c = atoi(argv[4]);  /* number of consumers        */
-
-	mq_unlink("/coolqueue");
- 	attr.mq_maxmsg = 10;
-    attr.mq_msgsize = 20;
-    queue_d = mq_open("/coolqueue", O_RDWR | O_CREAT, 0644, &attr);
-    printf("Open desc %d\n", queue_d);
-
-   
-	pini = fork();
-	if (pini<0){
-		return -1;
-	}else if ( pini == 0){
-    	/*spawner p*/
-    	printf("Producer spawner\n");
-    	pid_t pros_pids[num_p];
-
-    	for(int i = 0; i < num_p; i++){
-    		printf("I'm spawning producers\n");
-    		pros_pids[i] = fork();
-    		printf("Forked pros_pid is %d\n", pros_pids[i]);
-            if (pros_pids[i] == 0) {
-                printf("Producer\n");
-                producer(i);
-            } else {
-                return 0;
-            }
-    	}
-    } else {
-    	pid_t cini;
-    	cini = fork();
-    	if (cini<0){
-    		return -1;
-    	} else if (cini == 0){
-    		/*spwaner c*/
-    		printf("Consumer spwaner\n");
-            pid_t cons_pids[num_c];
-
-            for (int i = 0; i < num_c; i++) {
-                printf("I'm spawning consumers\n");
-                cons_pids[i] = fork();
-                printf("Forked cons_pid is %d\n", cons_pids[i]);
-                if (cons_pids[i] == 0) {
-                    consumer_id = i;
-                    printf("Consumer id %d spawned.\n", consumer_id);
-                    mqd_t recv = mq_open("/coolqueue", O_RDONLY);
-                    consumer(recv);
-                }
-            }
-    
-    	}else{
-    		/*parent main*/
-    	}
+    if (argc != 5) {
+        printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
+	exit(1);
     }
 
+    num = atoi(argv[1]);	/* number of items to produce */
+    maxmsg = atoi(argv[2]); /* buffer size                */
+    num_p = atoi(argv[3]);  /* number of producers        */
+    num_c = atoi(argv[4]);  /* number of consumers        */
 
     gettimeofday(&tv, NULL);
     g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
 
+    pid_t pros_pids[num_p];
+    pid_t cons_pids[num_c];
+
+    mq_unlink("/coolqueue");
+    struct mq_attr attr;
+    attr.mq_maxmsg = maxmsg;
+    attr.mq_msgsize = sizeof(int);
+    attr.mq_flags = 0;
+    attr.mq_curmsgs = 0;
+    queue_d = mq_open("/coolqueue", O_RDWR | O_CREAT, 0644, &attr);
+    printf("Open queue descriptor %d\n", queue_d);
+
+    for (int i = 0; i < num_p; i++) {
+        pros_pids[i] = fork();
+        if (pros_pids[i] == -1) {
+            printf("Failed to fork\n");
+            exit(1);
+        } else if (pros_pids[i] == 0) {
+            printf("Creating producer %d\n", i);
+            producer(i);
+        } 
+    }
+
+    for (int i = 0; i < num_c; i++) {
+        cons_pids[i] = fork();
+        if (cons_pids[i] == -1) {
+            printf("Failed to fork\n");
+            exit(1);
+        } else if (cons_pids[i] == 0) {
+            printf("Creating consumer %d\n", i);
+            consumer(queue_d, i);
+        }
+    }
+   
+    for (int i = 0; i < num_p; i++) {
+        waitpid(pros_pids[i], NULL, 0);
+    }
+
+    int killem = -1;
+    for (int i = 0; i < num_c; i++) {
+        mq_send(queue_d, (char *) &killem, sizeof(int), 0);
+    } 
+
+    for (int i = 0; i < num_c; i++) {
+        waitpid(cons_pids[i], NULL, 0);
+    }
 
     gettimeofday(&tv, NULL);
     g_time[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
@@ -112,31 +100,30 @@ int main(int argc, char *argv[])
     exit(0);
 }
 
-void producer(int i){	
+void producer(int prod_id){	
     mqd_t send = mq_open("/coolqueue", O_WRONLY);
     printf("Send desc %d\n", send);
 
-    char msg[num];
-    for (int j = i; j < num; j += num_p){
-    	sprintf(msg,"%d",j);
-    	mq_send(queue_d, msg, strlen(msg), 1);
+    for (int j = prod_id; j < num; j += num_p){
+    	mq_send(queue_d, (char *)&j, sizeof(int), 1);
     }
-
+    mq_close(send);
+    exit(0);
 }
 
-void consumer(mqd_t fd) {
+void consumer(mqd_t fd, int cons_id) {
     int received;
     int root;
     printf("Recv desc %d\n", fd);
-    char rcv[40];
-    while(1) {
-        mq_receive(fd, rcv, 40, NULL);
-        received = atoi(rcv);
-        printf("Consumer %d Received int %d", consumer_id, received);
-        root = sqrt(received);
+    int rcv;    
+    while(rcv != -1) {
+        mq_receive(fd, (char *)&rcv, sizeof(int), NULL);
+        received = rcv;
+        root = sqrt(rcv);
         if ((root * root) == received) {
-                printf("%d %d %d\n", consumer_id, received, root);
+            printf("%d %d %d\n", cons_id, received, root);
         }
     }
+    exit(0);
     perror(NULL);
 }
