97a15b9968d62f0f881d20d63b308babbae13e86
ConstanTineYoung <793565914@qq.com>
2018-12-02 00:35:43 -0500

    add 7 test cases

--- lab4/starter/main_test.c
+++ lab4/starter/main_test.c
@@ -1,5 +1,4 @@
-/**
-  * ECE254 Linux Dynamic Memory Management Lab
+/*E254 Linux Dynamic Memory Management Lab
   * @file: main_test.c
   * @brief: The main file to write tests of memory allocation algorithms
   */ 
@@ -20,40 +19,140 @@
 
 int main(int argc, char *argv[])
 {
-	int num = 0;
-	int algo = 0; // default algorithm to test is best fit  
+	char model = *argv[1];
+	int algo = atoi(argv[2]); 
 	void *p, *q;
+	int num = 0;
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <0/1>. 0 for best fit and 1 for worst fit \n", argv[0]);
-		exit (1);
-	} else if (!strcmp(argv[1], "1") || !strcmp(argv[1], "0")) {
-		algo = atoi(argv[1]);
-	} else {
-		fprintf(stderr, "Invalid argument, please specify 0 or 1\n");
-		exit(1);
-	}
-	
-	if ( algo == 0 ) {
-		best_fit_memory_init(1024);	// initizae 1KB, best fit
-
-		p = best_fit_alloc(8);		// allocate 8B
-		printf("best fit: p=%p\n", p);
-		if ( p != NULL ) {
-			best_fit_dealloc(p);	
+	if( model == 'b' ){
+		best_fit_memory_init(1024);
+		switch(algo){
+			case 0:
+				//Allocate full free space region
+				p = best_fit_alloc(1024);
+				printf("best fit: p=%p\n", p);
+				if ( p != NULL ) {
+				best_fit_dealloc(p);	
+				}
+				num = best_fit_count_extfrag(4);
+				break;
+			case 1:
+				//Allocate greater than initialised memory
+				p = best_fit_alloc(1025);
+				printf("best fit: p=%p\n", p);
+				if ( p != NULL ) {
+				best_fit_dealloc(p);	
+				}
+				num = best_fit_count_extfrag(4);
+				break;
+			case 2:
+				//Repeatedly allocate and deallocate same block while checking external fragmentation
+				while(1){
+					p = best_fit_alloc(256);
+					printf("best fit: p=%p\n", p);
+					if ( p != NULL ) {
+					best_fit_dealloc(p);	
+					}
+					num = best_fit_count_extfrag(4);
+					printf("num = %d\n", num);
+				} 
+				break;
+			case 3:
+				//Repeated allocations of a fixed size(small)
+				;int i = 12;
+				while(i>0){
+					p = best_fit_alloc(8);
+					printf("best fit: p=%p\n", p);
+					i--;
+				}
+				break;
+			case 4:
+				//Repeated allocations of a fixed size(large)
+				while(1){
+					p = best_fit_alloc(256);
+					printf("best fit: p=%p\n", p);
+				}
+				break;
+			case 5:
+				//Increasing size allocations
+				for(int i = 1; i < 512; i = 2*i ){
+					p = best_fit_alloc(i);
+					printf("best fit: p=%p\n", p);
+				}
+				break;
+			case 6:
+				//Decreasing size allocations
+				for(int i = 512; i > 1; i = i/2 ){
+					p = best_fit_alloc(i);
+					printf("best fit: p=%p\n", p);
+				}
+				break;
+			case 7:
+				//Dealloc invalid pointers
+				p = NULL;
+				best_fit_dealloc(p);
+				break;	
 		}
-		num = best_fit_count_extfrag(4);
-	} else if ( algo == 1 ) {
+		
 
-		worst_fit_memory_init(1024);	// initizae 1KB, worst fit
-
-		q = worst_fit_alloc(8);		// allocate 8B
-		printf("worst fit: q=%p\n", q);
-		if ( q != NULL ) {
-			worst_fit_dealloc(q);	
+	}else if( model == 'w' ){
+		worst_fit_memory_init(1024);
+		switch(algo){
+			case 0:
+				//Allocate full free space region
+				q = worst_fit_alloc(1024);
+				printf("worst fit: q=%p\n", q);
+				if ( q != NULL ) {
+					worst_fit_dealloc(q);
+				}
+				num = worst_fit_count_extfrag(4);
+				break;
+			case 1:
+				//Allocate greater than initialised memory
+				q = worst_fit_alloc(1025);
+				printf("worst fit: q=%p\n", q);
+				if ( q != NULL ) {
+					worst_fit_dealloc(q);
+				}
+				num = worst_fit_count_extfrag(4);
+				break;
+			case 3:
+				//Repeated allocations of a fixed size(small)
+				while(1){
+					p = worst_fit_alloc(8);
+					printf("worst fit: p=%p\n", p);
+				}
+				break;
+			case 4:
+				//Repeated allocations of a fixed size(large)
+				while(1){
+					p = worst_fit_alloc(256);
+					printf("worst fit: p=%p\n", p);
+				}
+				break;
+			case 5:
+				//Increasing size allocations
+				for(int i = 1; i < 512; i = 2*i ){
+					p = worst_fit_alloc(i);
+					printf("worst fit: p=%p\n", p);
+				}
+				break;
+			case 6:
+				//Decreasing size allocations
+				for(int i = 512; i > 1; i = i/2 ){
+					p = worst_fit_alloc(i);
+					printf("worst fit: p=%p\n", p);
+				}
+				break;
+			case 7:
+				//Dealloc invalid pointers
+				p = NULL;
+				worst_fit_dealloc(p);
+				break;	
 		}
-		num = worst_fit_count_extfrag(4);
-	} else {
+		
+
+	}else{
 		fprintf(stderr, "Should not reach here!\n");
 		exit(1);
 	}
@@ -62,3 +161,4 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
+
